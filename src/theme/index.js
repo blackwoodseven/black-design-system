659ee531d81b5be44264dba5774ae3709d12ed1b
José Luis Vizcaíno Martínez <jlvizmar@gmail.com>
2017-12-25 20:45:56 +0100

    Add theme

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,8 +1,29 @@
 const theme = {
   palette: {
-    mainColor: '#333'
+    black: '#000000' /* Black */ ,
+    purple: '#776FB2' /* Toolbox */ ,
+    gray: '#777777' /* Gray  */ ,
+    manatee: '#9A9A9A' /* Manatee */ ,
+    gainsboro: '#DDDDDD' /* Gainsboro */ ,
+    white: '#FFFFFF' /* White */ ,
+    verdigris: '#41AAA9' /* Verdigris */ ,
+    jasper: '#E33440' /* Jasper */ ,
+    mulberry: '#C65282' /* Mulberry */ ,
+    onyx: '#141414' /* Onyx */ ,
+    pastelOrange: '#FDBB54' /* Pastel orange */ ,
+    tealGreen: '#04714C' /* Teal green */ ,
+    etonBlue: '#80CFA9' /* Eton blue */ ,
+    turquoise: '#27CEBF' /* Turquoise */ ,
+    mediumTurqouise: '#54C3E1' /* Medium turquoise */ ,
+    orchid: '#CB80CF' /* Orchid */ ,
+    tickleMe: '#FF82AF' /* Tickle Me Pink */
   },
-  fontSize: {}
+  fontSize: {
+    small: 12,
+    medium: 16,
+    large: 24,
+    extraLarge: 32
+  }
 }
 
 export default theme
