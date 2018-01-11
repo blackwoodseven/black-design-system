e282f395526f601e9a5b1d330535d02c7ad3f74a
David Martinez <davidmartinez@blackwoodseven.com>
2018-01-11 12:26:09 +0100

    RaisedButton component

---
+++ src/components/RaisedButton/index.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import RaisedButton from 'material-ui/RaisedButton'
+
+export default RaisedButton
---
+++ src/components/RaisedButton/stories.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import { linkTo } from '@storybook/addon-links'
+
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+
+import RaisedButton from './index.js'
+
+storiesOf('RaisedButton', module).add('RaisedButton', () => (
+  <MuiThemeProvider>
+    <RaisedButton className="RaisedButton" />
+  </MuiThemeProvider>
+))
