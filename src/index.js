c29f42d0e7f1feaf504b80f7a106954cd65a6188
David Martinez <davidmartinez@blackwoodseven.com>
2018-01-11 12:22:06 +0100

    TextField component

--- src/index.js
+++ src/index.js
@@ -5,5 +5,6 @@ import ToolbarTitle from './components/ToolbarTitle'
 import FlatButton from './components/FlatButton'
 import Menu from './components/Menu'
 import MenuItem from './components/MenuItem'
+import TextField from './components/TextField'
 
-export { Paper, Toolbar, ToolbarGroup, ToolbarTitle, FlatButton, Menu, MenuItem }
+export { Paper, Toolbar, ToolbarGroup, ToolbarTitle, FlatButton, Menu, MenuItem, TextField }
