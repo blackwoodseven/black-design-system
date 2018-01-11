1d44100012f0dd3ef94e18fa8797452ba84f28c6
David Martinez <davidmartinez@blackwoodseven.com>
2018-01-11 12:27:39 +0100

    include RaisedButton on library index

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import Toolbar from './components/Toolbar'
 import ToolbarGroup from './components/ToolbarGroup'
 import ToolbarTitle from './components/ToolbarTitle'
 import FlatButton from './components/FlatButton'
+import RaisedButton from './components/RaisedButton'
 import Menu from './components/Menu'
 import MenuItem from './components/MenuItem'
 import TextField from './components/TextField'
 
-export { Paper, Toolbar, ToolbarGroup, ToolbarTitle, FlatButton, Menu, MenuItem, TextField }
+export { Paper, Toolbar, ToolbarGroup, ToolbarTitle, FlatButton, RaisedButton, Menu, MenuItem, TextField }
