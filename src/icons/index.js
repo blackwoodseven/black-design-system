16de8556f2afce7c045d2e73b1983bc212cf41bd
David Martinez <davidmartinez@blackwoodseven.com>
2018-03-14 12:00:47 +0100

    icons

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,3 +1,61 @@
+import AccountBox from 'material-ui/svg-icons/action/account-box'
 import Add from 'material-ui/svg-icons/content/add'
+import ArrowDropDown from 'material-ui/svg-icons/navigation/arrow-drop-down'
+import ArrowUpward from 'material-ui/svg-icons/navigation/arrow-upward'
+import AttachMoney from 'material-ui/svg-icons/editor/attach-money'
+import ChevronLeft from 'material-ui/svg-icons/navigation/chevron-left'
+import Close from 'material-ui/svg-icons/navigation/close'
+import DateRange from 'material-ui/svg-icons/action/date-range'
+import Delete from 'material-ui/svg-icons/action/Delete'
+import Done from 'material-ui/svg-icons/action/done'
+import EuroSymbol from 'material-ui/svg-icons/action/euro-symbol'
+import FileDownload from 'material-ui/svg-icons/file/file-download'
+import FilterList from 'material-ui/svg-icons/content/filter-list'
+import GetApp from 'material-ui/svg-icons/action/get-app'
+import GpsNotFixed from 'material-ui/svg-icons/device/gps-not-fixed'
+import Home from 'material-ui/svg-icons/action/home'
+import InfoOutline from 'material-ui/svg-icons/action/info-outline'
+import KeyboardArrowDown from 'material-ui/svg-icons/hardware/keyboard-arrow-down'
+import KeyboardArrowLeft from 'material-ui/svg-icons/hardware/keyboard-arrow-left'
+import KeyboardArrowRight from 'material-ui/svg-icons/hardware/keyboard-arrow-right'
+import KeyboardArrowUp from 'material-ui/svg-icons/hardware/keyboard-arrow-up'
+import MoreHoriz from 'material-ui/svg-icons/navigation/more-horiz'
+import MoreVert from 'material-ui/svg-icons/navigation/more-vert'
+import PermMedia from 'material-ui/svg-icons/action/perm-media'
+import Search from 'material-ui/svg-icons/action/search'
+import Sort from 'material-ui/svg-icons/content/sort'
+import Warning from 'material-ui/svg-icons/alert/warning'
+import ZoomIn from 'material-ui/svg-icons/action/zoom-in'
+import ZoomOut from 'material-ui/svg-icons/action/zoom-out'
 
-export { Add }
+export {
+  AccountBox,
+  Add,
+  ArrowDropDown,
+  ArrowUpward,
+  AttachMoney,
+  ChevronLeft,
+  Close,
+  DateRange,
+  Delete,
+  Done,
+  EuroSymbol,
+  FileDownload,
+  FilterList,
+  GetApp,
+  GpsNotFixed,
+  Home,
+  InfoOutline,
+  KeyboardArrowDown,
+  KeyboardArrowLeft,
+  KeyboardArrowRight,
+  KeyboardArrowUp,
+  MoreHoriz,
+  MoreVert,
+  PermMedia,
+  Search,
+  Sort,
+  Warning,
+  ZoomIn,
+  ZoomOut
+}
